f0fd019764092e22640c7d712610538866e268eb
AEE <alebrahim.ali@gmail.com>
2023-06-25 01:36:06 -0700

    Updates to inital work, runs on streamlit
    - removed old code that had Agents on streamlit session
    - excellent work Shane with the API
    - front end cleanup remains
    - 3rd page and history page remain
    
    Co-Authored-By: spolisar <spolisar@users.noreply.github.com>

--- Netbox/Device_Discovery/Netbox_App.py
+++ Netbox/Device_Discovery/Netbox_App.py
@@ -1,26 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 14 00:37:29 2022
+Main Page - Netbox Device Discovery
 
-@author: alebr
+@author: Shane Polisar, aolabs.ai
 """
-
-# AO Labs Modules
-# modified import to import ao_core 
-import sys
-sys.path.append('/home/shane/Development/Animo-Omnis')
-import ao_core as ao
 
 # 3rd Party Modules
 import numpy as np
 import streamlit as st
-
-#api requests
 import requests
+
 
 #using preset api key for now
 api_key = 'buildBottomUpRealAGI'
 st.session_state.api_key = api_key
+
 
 def agent_api_call(agent_id, input_dat, api_key, label=None):
     url = "https://7svo9dnzu4.execute-api.us-east-2.amazonaws.com/v0dev/kennel/agent"
@@ -46,17 +40,6 @@ def agent_api_call(agent_id, input_dat, api_key, label=None):
     return response
 
 
-# Application-Specific Modules
-# import pynetbox as pynb
-
-# nb = pynb.api(
-#     'https://demo.netbox.dev',
-#     token='18001f2de10ec5bae0d1d2e2614e6f98d427a434',
-#     threading=True,
-# )
-
-from netbox_ai_app.main_netbox import deviceToIO
-
 from PIL import Image
 from urllib.request import urlopen
 url2 = "https://i.imgur.com/j3jalQE.png"
@@ -74,73 +57,49 @@ st.set_page_config(
     }
 )
 
+# app front end
 st.title('Netbox Demo - powered by aolabs.ai')
 st.write("")
 st.markdown("## Add Your Netbox Account")
 st.markdown("Please enter the information below and then click the **Add Netbox Account** button to get started with this demo.")
 
-# st.session_state.nb_USER_url = st.text_input('Enter your Netbox account URL:')
-st.session_state.nb_USER_url = st.text_input('Enter your Netbox account URL:', "https://demo.netbox.dev/")
-st.session_state.nb_USER_api_token = st.text_input('Enter your Netbox account API token:')
-st.session_state.USER_num_test_devices = st.number_input('How many of the devices in this Netbox account would you like to use to test this new Device Discovery Augmentation service?' , 0, 30)
-st.session_state.agent_id = st.text_input('enter the id of your agent')
+# USER inputs
+st.session_state.nb_USER_url = st.text_input('Enter your Netbox account URL:', "https://demo.netbox.dev/")    ## Note to Shane -- well done with the default text here! while looking up how you were using st.text_input, i see they also have a password option, see line below
+st.session_state.nb_USER_api_token = st.text_input('Enter your Netbox account API token:',  type="password")
+st.session_state.USER_num_total_devices = st.number_input('How many of the devices in this Netbox account would you like to use for this demo?' , 0, 200)
+x = st.session_state.USER_num_total_devices
+st.session_state.USER_num_test_devices = st.number_input("Of those ("+str(x)+") devices, how many should be withheld to TEST this new local Device Discovery AI?" , 0, 100)
+st.session_state.agent_id = st.text_input("Enter a unique name/id for this AI Agent")
 
 
-def New_NB_ACCOUNT_Callback():
-
-# if 'nbd' not in st.session_state:
+if st.button("Add Netbox Account & Train Agent", type="primary"):
+    
     import pynetbox
     nb = pynetbox.api(
         st.session_state.nb_USER_url,
-        token=st.session_state.nb_USER_api_token,
+        token= str(st.session_state.nb_USER_api_token),
         threading=True,
     )
-    devices = list(nb.dcim.devices.all())
+    devices = list(nb.dcim.devices.all())[0: st.session_state.USER_num_total_devices ]
 
-    #dictionary comprehensions that map ids to strings
-    # dict_manufacturer = {d.device_type.manufacturer.id:d.device_type.manufacturer for d in devices}
-    # dict_device_type = {d.device_type.id:d.device_type for d in devices}
-    # dict_sites = {d.sites.id:d.site for d in devices}
-    manufacturers = {}
-    device_types = {}
-    sites = {}
-    roles = {}
-    for d in devices:
-        manufacturers[d.device_type.manufacturer.id] = d.device_type.manufacturer.__str__()
-        device_types[d.device_type.id] = d.device_type.__str__()
-        sites[d.site.id] = d.site.__str__()
-        roles[d.device_role.id] = d.device_role.__str__()
-    
-    st.session_state.manufacturers = manufacturers
-    st.session_state.device_types = device_types
-    st.session_state.sites = sites
-    st.session_state.roles = roles
-
-    
-    
-    
-    # test_devices_in = np.random.choice(len(devices), st.session_state.USER_num_test_devices, replace=False)
-    # test_devices_in = np.random.choice(devices, st.session_state.USER_num_test_devices, replace=False)
-    # train_devices_in = np.delete( np.arange(len(devices)), test_devices_in)
-    # train_devices_in = np.setdiff1d(devices, test_devices_in)
-    # choices = np.random.choice(len(devices), st.session_state.USER_num_test_devices, replace=False)
-    # print(choices)
-    # test_devices_in = np.copy(devices[choices])
-    # train_devices_in = np.delete(devices, choices)
+    #dictionary comprehensions that map ids to strings, for device name, mfg, type, site, and role
+    st.session_state.device_ids    = {d.id : d.__str__() for d in devices}    #  to map device id to device name
+    st.session_state.manufacturers = {d.device_type.manufacturer.id : d.device_type.manufacturer.__str__() for d in devices}
+    st.session_state.device_types  = {d.device_type.id : d.device_type.__str__() for d in devices}
+    st.session_state.sites         = {d.site.id : d.site.__str__() for d in devices}
+    st.session_state.device_roles  = {d.device_role.id : d.device_role.__str__() for d in devices}
+        
+    # for demo performance snapshot, seperate into training and testing sets
     test_size = st.session_state.USER_num_test_devices
     np.random.shuffle(devices)
     test_devices_in = devices[:test_size]
     train_devices_in = devices[test_size:]
-
     st.session_state.test_devices_in = test_devices_in
     
-
-    # print(devices[0])
     # call API in a loop for all TRAIN devices with labels 
     count = 0
     for d in train_devices_in:
         INPUT = format(d.device_type.manufacturer.id, '010b') + format(d.device_type.id, '010b') + format(d.site.id, '010b')
-        # TypeError: unsupported format string passed to DeviceTypes.__format__
         LABEL = format(d.device_role.id, '010b')
 
         # call API
@@ -149,152 +108,46 @@ def New_NB_ACCOUNT_Callback():
         print("trained on device {} with status code {}".format(count, response))
         count += 1
     
-    # display training is DONE message
-    st.markdown('# Training done')
-    # prompt click from user to move along (to next page or a button appears on for loop completion appears)
-
     # call API in loop for all TEST devices WITHOUT LABELS
-        # similar to above
-        
-    test_devices_out = np.arange(len(train_devices_in))
+    test_devices_out_roles = list(range(len(test_devices_in)))
     correct_count = 0
-    for i in range(len(test_devices_in)):
+    noguess_count = 0
+    for i in list(range(len(test_devices_in))):
         d = test_devices_in[i]
-    # for d in test_devices_in:
         INPUT = format(d.device_type.manufacturer.id, '010b') + format(d.device_type.id, '010b') + format(d.site.id, '010b')
+
+        # call API
         response = agent_api_call(st.session_state.agent_id, INPUT, api_key)
         story = response.json()['story']
-        np.append(test_devices_out, story)
 
         expected = format(d.device_role.id, '010b')
         if expected == story:
             correct_count += 1
+        # to convert binary output of Agent back to role ID and, via the role dict, the role name
+        try: 
+            test_devices_out_roles[i] = st.session_state.device_roles[int(story, 2)]
+        except KeyError:  # if role ID guessed by Agent does not exist
+            test_devices_out_roles[i] = "NO GUESS"
+            noguess_count += 1    
 
     # save accuracy and other info in session to display to netbox visitor
+    st.session_state.test_devices_out_roles = test_devices_out_roles
     st.session_state.correct_count = correct_count
+    st.session_state.noguess_count = noguess_count
+
+    st.markdown('# Training done')
     
-    # make it all look pretty and explainable
-    st.markdown('# predicted correctly for {count}/{total}'.format(count=correct_count, total=st.session_state.USER_num_test_devices))
-    st.markdown('# which is an accuracy of {}%'.format((correct_count/st.session_state.USER_num_test_devices)*100))
-    #table of devices, correct labels, and guessed labels?
-
-    # prompt visitor to clone this app and associated agent
-    
-    ## next up
-    # add history vibility
-        # raw and per neuron, see how it's displayed in basic clam demo app
-    
-    ## bonus
-    # add new devices, on click of button, re-trains agent
-
-#%%
-def deviceToIO(d):
-    info = {
-        'id': d.id,
-        'idBin': format(d.id, '010b'),
-        'type': d.device_type.id,
-        'typeBin': format(d.device_type, '010b'),
-        'manufacturer': d.device_type.manufacturer.id,
-        'manufacturerBin': format(d.device_type.manufacturer.id, '010b'),
-        'site': d.site.id,
-        'siteBin': format(d.site.id, '010b'),
-        'role': d.device_role.id,
-        'roleBin': format(d.device_role.id, '010b'),
-    }
-    return info
-
-#%%
-        
-    
-    arraySize = 10
-    
-    # map binary strings to ids
-    roles = {}
-    manufacturers = {}
-    sites = {}
-    types = {}
-    roles_id_to_str = {}
-    
-    devices_array_IO = np.zeros(4000, dtype='O')
-    devices_info = np.zeros([4000, 5], dtype='O')
-        
-    inc = 0
-    #populate devices array
-    for device in devices:
-
-        devices_array_IO[inc] = deviceToIO(device, arraySize)
-        
-        devices_info[inc, 0] = str(device)
-        devices_info[inc, 1] = str(device.device_type.manufacturer)
-        devices_info[inc, 2] = str(device.site)
-        devices_info[inc, 3] = str(device.device_type)
-        devices_info[inc, 4] = str(device.device_role)
-   
-        #add device data to sets so they can be options in text boxes
-        roles[str(device.device_role)] = device.device_role.id
-        manufacturers[str(device.device_type.manufacturer)] = device.device_type.manufacturer.id
-        sites[str(device.site)] = device.site.id
-        types[str(device.device_type)] = device.device_type.id
-    
-        roles_id_to_str[device.device_role.id] = str(device.device_role)    
-    
-        # count number of devices
-        inc += 1
-
-    devices_array_IO = devices_array_IO[0:inc]
-    devices_info = devices_info[0:inc, :]
-
-    test_devices_in = np.random.choice(inc, st.session_state.USER_num_test_devices, replace=False)    
-    train_devices_array_IO = np.delete(devices_array_IO, test_devices_in)
-    test_devices_array_IO = np.copy(devices_array_IO[test_devices_in])    
-    test_devices_info = np.copy(devices_info[test_devices_in, :])
-               
-    nbd = {
-        "roles": roles,
-        "manufacturers" : manufacturers,
-        "sites" : sites,
-        "types" : types,
-        "roles_id_to_str" : roles_id_to_str,
-        "inc" : inc,
-        "train_devices_array_IO" : train_devices_array_IO,
-        "test_devices_array_IO" : test_devices_array_IO,
-        "test_devices_info" : test_devices_info,
-        }
-
-# if 'agent' not in st.session_state:
-    # configure architecture
-    from netbox_ai_app import arch_netbox
-    arch = arch_netbox.arch
-
-    # create agent
-    arch.full_conn()
-    agent = ao.Agent(arch)
-
-    #train agent
-    for d in train_devices_array_IO:
-        agent.next_state(d['manufacturerBin'] + d['siteBin'] + d['typeBin'],
-                    d['roleBin'], print_result=False, unsequenced=True)
-        agent.reset_state()
-
-    #save data to session
-
-    st.session_state.nbd = nbd
-
-    st.session_state.agent = agent    
-
-    st.session_state.mistakes = 0
-    st.session_state.mistakes_batch = 0
-    st.session_state.recs = 0
-    st.session_state.test_devices_roles_PREDICTED = 0
-
-
-st.write("")
-st.button("Add Netbox Account", on_click= New_NB_ACCOUNT_Callback)
-
-
-if 'nbd' not in st.session_state:
-    pass
-else:
+    # Note to Shane -- couldn't get the 2 markdowns below to work, please double chjeck and complete design of this area
+#    st.markdown('# predicted correctly for {count}/{total}'.format(count=correct_count, total=st.session_state.USER_num_test_devices))
+#    st.markdown('# which is an accuracy of {}%'.format((correct_count/st.session_state.USER_num_test_devices)*100))
+    #table of devices, correct labels, and guessed labels? -- Note to Shane -- yes, plus the device mfg, type, and site; see "device_discovery" object in page 2
+     # so redesign all the below, all the front end elements       
     st.write("Data successfully loaded, agent is trained from "+st.session_state.nb_USER_url+" via API token: "+st.session_state.nb_USER_api_token)  
-    st.write("There are "+str(st.session_state.nbd['inc'])+" devices; "+str(st.session_state.USER_num_test_devices)+" devices were withheld from the agent as test devices.")
+    st.write("There are "+str(count)+" devices; "+str(st.session_state.USER_num_test_devices)+" devices were withheld from the agent as test devices.")
     st.write("Please remember to re-click the button below if you change the NB account or number of test devices.")
+
+    st.session_state.nb_account_added = True
+    st.session_state.device_discovery = False   # so that device discovery results are refreshed on re-run
+
+else:
+    st.write("Add your info to run an Agent")
--- Netbox/Device_Discovery/pages/02_Add_New_Devices_(API_Batch).py
+++ Netbox/Device_Discovery/pages/02_Add_New_Devices_(API_Batch).py
@@ -1,84 +1,67 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Apr  9 01:58:15 2023
+Page 2 of App - Netbox Device Discovery
 
-@author: alebr
+Purpose: to display the results of an Agent's deivce role predictions
+
 """
-
-# # AO Labs Modules
-# import ao_core as ao
 
 # 3rd Party Modules
 import numpy as np
 import streamlit as st
 import pandas as pd
 
-# from netbox_ai_app.main_netbox import binaryListToDecimal
 
-#show results button from first page?
-
-
-if 'nbd' not in st.session_state: st.text("You have to connect your Netbox account first.")
-
+if 'nb_account_added' not in st.session_state: st.text("You have to connect your Netbox account first.")
 else:
-    # load session data
-    roles = st.session_state.nbd["roles"]
-    manufacturers = st.session_state.nbd['manufacturers']
-    sites = st.session_state.nbd['sites']
-    types = st.session_state.nbd['types']
-    roles_id_to_str = st.session_state.nbd['roles_id_to_str']
-    inc = st.session_state.nbd['inc']
-    test_devices_info = st.session_state.nbd['test_devices_info']
-    test_devices_array_IO = st.session_state.nbd['test_devices_array_IO']
-    train_devices_array_IO = st.session_state.nbd['train_devices_array_IO']
+
+# variables loaded from st.session_state (client session)#####################
+#########
+    USER_num_test_devices = st.session_state.USER_num_test_devices
+    test_devices_in =       st.session_state.test_devices_in
+    test_devices_out_roles= st.session_state.test_devices_out_roles    
     
-    USER_num_test_devices = test_devices_array_IO.shape[0]
-    
-    
+    correct_count = st.session_state.correct_count
+    noguess_count = st.session_state.noguess_count
+#########
+
+
+    # app front end
     st.title('Netbox Demo - powered by aolabs.ai')
     st.write("")
     st.markdown("## Programmatically Add New Devices")
-            
-    def Batch_New_Devices_Callback():
-        #run agent on test data
-        i=0
-        st.session_state.test_devices_roles_PREDICTED = np.zeros(USER_num_test_devices, dtype='O')
-        for d in test_devices_array_IO:
-            st.session_state.agent.reset_state()
-            st.session_state.agent.next_state(d['manufacturerBin'] + d['siteBin'] + d['typeBin'],
-                                              print_result=False, unsequenced=True)
-            st.session_state.agent.next_state(d['manufacturerBin'] + d['siteBin'] + d['typeBin'],
-                                              print_result=False, unsequenced=True)
-        
-            recs_bin = st.session_state.agent.story[ st.session_state.agent.state-1,  st.session_state.agent.arch.Z__flat]
-            recs_dec = binaryListToDecimal(recs_bin.astype(int))
-            
-            try:
-                st.session_state.test_devices_roles_PREDICTED[i] = roles_id_to_str[recs_dec]
-            except KeyError:
-                st.session_state.mistakes_batch += 1
-                st.session_state.test_devices_roles_PREDICTED[i] = 'NO GUESS'
+
     
+    # create device_discovery to contain table of results
+    if st.session_state.device_discovery is False:
+        device_discovery = np.zeros([USER_num_test_devices, 6], dtype="O")
+        i= 0    
+        for d in test_devices_in:
+            device_discovery[i, 0] = d.__str__()                           # name
+            device_discovery[i, 1] = d.device_type.manufacturer.__str__()  # manufacturer
+            device_discovery[i, 2] = d.device_type.__str__()               # device type
+            device_discovery[i, 3] = d.site.__str__()                      # site 
+            device_discovery[i, 4] = ""                          # PREDICTED role
+            device_discovery[i, 5] = d.device_role.__str__()               # role, actual
             i += 1
-
-    st.button("Add this batch of new (test) devices", on_click= Batch_New_Devices_Callback)
-
-    device_discovery = np.zeros([USER_num_test_devices, 6], dtype="O")
-    device_discovery[:, 0] = test_devices_info[:, 0]  # name
-    device_discovery[:, 1] = test_devices_info[:, 1]  # manufacturer
-    device_discovery[:, 2] = test_devices_info[:, 2]  # site 
-    device_discovery[:, 3] = test_devices_info[:, 3]  # type 
-    device_discovery[:, 4] = st.session_state.test_devices_roles_PREDICTED
-    device_discovery[:, 5] = test_devices_info[:, 4]  # role, actual
-
-    device_discovery_df = pd.DataFrame( device_discovery, columns=['Name', 'Manufacturer', 'Site', 'Type', 'PREDICTED ROLE', 'role'])
+########   Save it to st.session_state
+        st.session_state.device_discovery = device_discovery
     
+        
+    # On USER click, return results from st.session_state from the main app page    
+    if st.button("Add this batch of new devices (i.e. run/view test)", key="view_results", type="primary"):
+    
+        # Add results to 5th column, PREDICTED ROLE
+        st.session_state.device_discovery[:, 4] = st.session_state.test_devices_out_roles
+        
+        # Display % accuracy + no guesses and other info
+        correct_percentage =   ( correct_count / USER_num_test_devices ) * 100
+        noguess_percentage = ( noguess_count / USER_num_test_devices ) * 100
+        
+        st.write("Out of "+str(USER_num_test_devices)+" devices added, the role was predicted correctly "+str(correct_count)+" times out of "+str(USER_num_test_devices)+".")
+        st.write("Or "+str(correct_percentage)+" %.")
+        st.write("Also, there were no predictions "+str(noguess_count)+" times, or "+str(noguess_percentage)+" %.")
+
+    # use pandas dataframe + streamlit to display results for USER
+    device_discovery_df = pd.DataFrame( st.session_state.device_discovery, columns=['Name', 'Manufacturer', 'Type', 'Site', 'PREDICTED ROLE', 'Role'])        
     st.write(device_discovery_df)
-    
-    correct_predicitons = sum(device_discovery[:, 4] == device_discovery[:, 5])
-    correct_percentage = (correct_predicitons/USER_num_test_devices) * 100
-    noguesses_percentage = (st.session_state.mistakes_batch/USER_num_test_devices) * 100
-
-    st.write("Out of "+str(USER_num_test_devices)+" devices added, the role was predicted correctly "+str(correct_predicitons)+" times out of "+str(USER_num_test_devices)+".")
-    st.write("Or "+str(correct_percentage)+" %.")
-    st.write("Also, there were no predictions "+str(st.session_state.mistakes_batch)+" times, or "+str(noguesses_percentage)+" %.")    
