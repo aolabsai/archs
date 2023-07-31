3e7ffbf756d9510530ba916e273d0459bbe0c2c8
AEE <alebrahim.ali@gmail.com>
2023-07-30 18:20:50 -0700

    added history to Agent script

--- Applications/Netbox/Device_Discovery/Agent_Script.py
+++ Applications/Netbox/Device_Discovery/Agent_Script.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Script to Run Device Discovery AI Agents for Self-Host NetBox
+a local version of this app: https://aolabs-netbox.streamlit.app/
 
 - run this script in the env where you host your NetBox instance; it uses the pynetbox module to connect to your device info only
 - Agents are persistently hosted on our Agents-as-a-Service API, reference https://docs.aolabs.ai/reference/agentinvoke
@@ -19,7 +20,7 @@ def agent_api_call(agent_id, input_data, label=None):
 
     url = "https://7svo9dnzu4.execute-api.us-east-2.amazonaws.com/v0dev/kennel/agent"
     payload = {
-        "kennel_id": "v0dev/TEST-Netbox_DeviceDiscovery",
+        "kennel_id": "v0dev/TEST-Netbox_DeviceDiscovery",  # using this arch https://github.com/aolabsai/archs/blob/main/Architectures/netbox-device_discovery.py
         "agent_id": agent_id,
         "INPUT": input_data,
         "control": {
@@ -41,7 +42,7 @@ def agent_api_call(agent_id, input_data, label=None):
 # Getting Devices list from NetBox
 nb = pynetbox.api(
     "https://demo.netbox.dev/",
-    token= "a112ecc9164506381125748adad1943c71e87939", # insert API token here as a string, get from https://demo.netbox.dev/user/api-tokens/
+    token= "a5d05d1136c262036d0525de49df3f59d231b7c1", # insert API token here as a string, get from https://demo.netbox.dev/user/api-tokens/ use username: admin password: admin
     threading=True)
 try:
     devices = list(nb.dcim.devices.all())
@@ -52,7 +53,7 @@ discovered_devices = []
 
 
 # Name your Agent; you can create as many as you need; Agents are not pre-trained
-agent_id = "Netreplica Test Agent"
+agent_id = "Test Agent 1"
 
 
 # Training your Agent on Devices list
@@ -83,4 +84,32 @@ print("Role ID response: " +str( int(response, 2) ))
 # Need to re-train Agent? Any time label is passed through, Agent learns it
 # Need more than 3 input fields for your *platform* use-case? This Agent was configured with 3 inputs to 1 output; its configuration is easy to expand and can be found here: https://github.com/aolabsai/archs/blob/main/Architectures/netbox-device_discovery.py
 
-# Agents are fully interpertable (not a blackbox), as they store all past states in their lookup table (only states labelled by a label or instinct trigger are used for learning; the rest is pruned)
+
+# Agents are also fully interpertable (not a blackbox) by design, as they store all past states in their lookup table (only states labelled by a label or instinct trigger are used for learning; the rest is pruned)
+# you can view the history below
+def retreive_agent_history(agent_id):
+
+    url = "https://7svo9dnzu4.execute-api.us-east-2.amazonaws.com/v0dev/kennel/agent"
+    payload = {
+        "kennel_id": "v0dev/TEST-Netbox_DeviceDiscovery",
+        "agent_id": agent_id,
+        "request": "story",
+        "control": {
+            "US": True
+        }
+    }
+    headers = {
+        "accept": "application/json",
+        "content-type": "application/json",
+        "X-API-KEY": "buildBottomUpRealAGI"
+    }
+    response = requests.post(url, json=payload, headers=headers)
+    response = response.json()
+    return response
+
+history = retreive_agent_history(agent_id)
+
+import numpy as np
+history_array = np.asarray(list( history['story']), dtype=int).reshape( 74, history['state'])     # 74 is the total number of neurons in this Agent, as per it's arch https://github.com/aolabsai/archs/blob/main/Architectures/netbox-device_discovery.py
+
+
